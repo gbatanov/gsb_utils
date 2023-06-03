c55c109cbd517675d4adcd925fe566210afdb481
gbatanov <gbatanov@yandex.ru>
2023-06-03 12:24:39 +0300

    v0.11.39 Channel

--- Channel.h
+++ Channel.h
@@ -21,35 +21,62 @@ public:
     }
     ~Channel()
     {
+        stopped = true;
+    }
+    void stop()
+    {
+        stopped = true;
         cv_r.notify_all();
         cv_w.notify_all();
     }
-
     void write(T msg)
     {
+        if (stopped)
+            return;
+
         if (Msg_.size() >= size_)
         {
             std::unique_lock<std::mutex> ul(mtx_w);
-            cv_w.wait(ul, [this]()
-                      { return Msg_.size() < size_; });
+            while (Msg_.size() >= size_ && !stopped)
+            {
+                if (std::cv_status::timeout == cv_w.wait_for(ul, std::chrono::milliseconds(100)))
+                {
+                    if (stopped)
+                        break;
+                }
+            }
             ul.unlock();
         }
+        if (stopped)
+            return;
         Msg_.push_back(msg);
         cv_r.notify_one();
     }
 
     T read()
     {
+        T msg{};
+        if (stopped)
+            return msg;
+
         if (Msg_.size() == 0)
         {
             std::unique_lock<std::mutex> ul(mtx_r);
-            cv_r.wait(ul, [this]()
-                      { return Msg_.size() > 0; });
+            while (Msg_.size() == 0 && !stopped)
+            {
+                if (std::cv_status::timeout == cv_r.wait_for(ul, std::chrono::milliseconds(100)))
+                {
+                    if (stopped)
+                        break;
+                }
+            }
             ul.unlock();
         }
-        T msg = Msg_.front();
+        if (stopped)
+            return msg;
+        msg = Msg_.front();
         Msg_.erase(Msg_.begin());
-        cv_w.notify_one();
+
         return msg;
     }
 
@@ -58,6 +85,7 @@ private:
     uint64_t size_ = 0;
     std::mutex mtx_r, mtx_w;
     std::condition_variable cv_r, cv_w;
+    bool stopped = false; // channel closed
 };
 
 #endif
